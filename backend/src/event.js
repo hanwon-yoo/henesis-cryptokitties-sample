2132dd96c2ec239891dd0a28b83354fd0439e3cf
Youngtaek Oh <oyt311@hanmail.net>
2019-09-04 13:19:34 +0900

    answer for event.js

--- backend/src/event.js
+++ backend/src/event.js
@@ -1,7 +1,39 @@
 import Henesis from '@haechi-labs/henesis-sdk-js'
 
 export default async function ({config, model}) {
-    /*
-        Your code starts here!
-    */
+    const henesis = new Henesis();
+    const subscription = await henesis.subscribe(
+        config.integrationId,
+        "streamedBlock"
+    )
+    subscription.on('message', async (message) => {
+        const event = messageToEvent(message)
+        model.save(event)
+        console.log(`data received, event:${event.event}`)
+        message.ack();
+    });
+
+    subscription.on('error', err => {
+        console.error(err);
+    });
+
+    //parsing logic
+    function messageToEvent(message) {
+        const event = message.data[0].events[0];
+        const blockMeta = message.data[0].blockMeta
+        return {
+            event: event.eventName.split('(')[0],
+            contract: event.contractName,
+            transactionHash: event.transaction.hash,
+            args: dataToArgs(event.data),
+            blockMeta
+        }
+        function dataToArgs(data) {
+            const res = {}
+            for (let item of data) {
+                res[item.name] = item.value
+            }
+            return res
+        }
+    }
 }
